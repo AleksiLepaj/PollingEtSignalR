ec878f059c08b622949343994c55cad4c2d34978
Alexis <2175497@cegepmontpetit.ca>
2024-09-13 10:08:29 -0400

    FCT Polling
    Complétion du polling dans l'application

--- ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UselessTask } from '../models/UselessTask';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-polling',
@@ -9,26 +10,30 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class PollingComponent implements OnInit {
 
+  apiUrl = "http://localhost:7289/api/";
   title = 'labo.signalr.ng';
   tasks: UselessTask[] = [];
   taskname: string = "";
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.updateTasks();
   }
 
-  complete(id: number) {
+  async complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
+    return await lastValueFrom(this.http.get<any>(this.apiUrl + 'UselessTasks/Complete/' + id));
   }
 
-  addtask() {
+  async addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    this.tasks.push(await lastValueFrom(this.http.post<UselessTask>(this.apiUrl + 'UselessTasks/Add?taskText=' + this.taskname, null)));
   }
 
-  updateTasks() {
+  async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    this.tasks = await lastValueFrom(this.http.get<UselessTask[]>(this.apiUrl + 'UselessTasks/GetAll'));
   }
 }
