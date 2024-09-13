a2f9868cfa7bc8add64c066fca83e5c560a6beff
Alexis <2175497@cegepmontpetit.ca>
2024-09-13 10:30:16 -0400

    Progress

---
+++ PollingEtSignalR/labo.signalr.api/Hub/TaskHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using labo.signalr.api.Data;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace labo.signalr.api.Hub
+{
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+
+    public class TaskHub : Hub
+    {
+        ApplicationDbContext _context;
+
+        public TaskHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+        }
+    }
+}
